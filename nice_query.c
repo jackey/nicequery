dbf5d7cb615bc0f47626bab9bf3f9b16de1b467d
ziwen-web <jziwenchen@gmail.com>
2016-02-20 11:06:39 +0800

    c

--- nice_query.c
+++ nice_query.c
@@ -5,7 +5,7 @@
 #include "php.h"
 #include "php_nice_query.h"
 
-static function_entry nice_query_functions[] = {
+static zend_function_entry nice_query_functions[] = {
 	PHP_FE(hello_world, NULL),
 	{NULL, NULL, NULL}
 };
